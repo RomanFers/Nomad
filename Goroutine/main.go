b948b4eaf37add38573ee5abbab747c06e6d1bac
RomanFers <87385337+RomanFers@users.noreply.github.com>
2021-07-13 20:05:51 +0300

    Add files via upload

---
+++ Goroutine/main.go
@@ -0,0 +1,36 @@
+//вывод размера веб-страниц в byte
+package main
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+type Page struct {
+	URL string
+	Size int
+}
+func responseSize(url string, channel chan Page) {
+	//	fmt.Println("Получающий", url)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	channel <- Page{URL: url, Size: len(body)}
+}
+func main() {
+	pages := make(chan Page)
+	urls := []string{"https://example.com/","https://golang.org/", "https://golang.org/doc","https://metanit.com/go/tutorial/6.3.php","https://www.wildberries.ru/"}
+	for _, url := range urls {
+		go responseSize(url, pages)
+	}
+	for i := 0; i < len(urls); i++ {
+		page := <-pages
+		fmt.Printf("URL: %s  Size: %d bytes \n", page.URL, page.Size)
+	}
+}
